281f58aaeebe866e91ca930d654e1d35c25cbda9
ibaG20 <marretogabriella@gmail.com>
2023-11-19 15:05:26 -0300

    correcao no arquivo db

--- config/db_sequelize.js
+++ config/db_sequelize.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('web2_db', 'postgres', 'root',
+const sequelize = new Sequelize('trab2', 'postgres', 'root',
         { host: 'localhost', dialect: 'postgres' });
 
 var db = {};
@@ -15,7 +15,7 @@ db.Comentario = require('../models/comentario.js')
         (sequelize, Sequelize);
 
 //falta fazer os relacionamentos aqui:
-Postagem.hasMany(db.Comentario, {
+db.Postagem.hasMany(db.Comentario, {
         foreignKey: 'postagemId',
         onDelete: 'NO ACTION'
 });
