711b65666f241865efd3d9f298986f2b556679af
ibaG20 <marretogabriella@gmail.com>
2023-11-26 19:43:43 -0300

    add swagger pra postagem

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const routes = require('./routers/route');
 const express = require('express');
+const swaggerUI = require('swagger-ui-express')
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const swaggerDocument  =require('./swagger.json')
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.use(routes);
 
 app.use(
